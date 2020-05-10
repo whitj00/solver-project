1372653f3ad17d4f8b65c47d2da4c8909146fc63
Whit Jack <whit@whitjack.me>
2020-05-09 21:03:22 -0700

    Formatting

--- ProjectInterpreter.fs
+++ ProjectInterpreter.fs
@@ -2,68 +2,75 @@ module ProjectInterpreter
 
 open ProjectParser
 
-let truthy (v : Expr) : bool =
+let truthy (v: Expr): bool =
     match v with
     | Bool b -> b
     | Num n -> n > 0
     | _ -> failwith "Value is not truthy/falsy"
- 
-let rec evalAnd (al : Expr list) : bool =
-    if List.length al = 0 then
-        true
+
+let rec evalAnd (al: Expr list): bool =
+    if List.length al = 0 then true
+    else if truthy (List.head al) then evalAnd (List.tail al)
+    else false
+
+let rec evalOr (ol: Expr list): bool =
+    if List.length ol = 0 then false
+    else if truthy (List.head ol) then true
+    else evalOr (List.tail ol)
+
+let rec evalIf (il: Expr list): Expr =
+    if List.length il <> 3
+    then failwith "If-statements must have a statement and two clauses"
+    else if truthy il.[0]
+    then il.[1]
+    else il.[2]
+
+let rec multiply nums =
+    if List.isEmpty nums then
+        1
     else
-        if truthy (List.head al) then
-            evalAnd (List.tail al)
-        else false
+        match List.head nums with
+        | Num n -> n * (multiply (List.tail nums))
+        | _ -> failwith "Invalid number"
 
-let rec evalOr (ol : Expr list) : bool =
-    if List.length ol = 0 then
-        false
+let rec add nums =
+    if List.isEmpty nums then
+        0
     else
-        if truthy (List.head ol) then true
-        else evalOr (List.tail ol)
+        match List.head nums with
+        | Num n -> n + (multiply (List.tail nums))
+        | _ -> failwith "Invalid number"
 
-let rec evalIf (il : Expr list) : Expr =
-    if List.length il <> 3 then
-        failwith "If-statements must have a statement and two clauses"
+let rec divide nums =
+    if List.isEmpty nums then
+        1
     else
-        if truthy il.[0] then il.[1]
-        else il.[2]
+        match List.head nums with
+        | Num n -> n / (divide (List.tail nums))
+        | _ -> failwith "Invalid number"
 
-let rec multiply nums = 
-    if List.isEmpty nums then 1
-    else match List.head nums with
-    | Num n -> n * (multiply (List.tail nums))
-    | _ -> failwith "Invalid number"
-
-let rec add nums = 
-    if List.isEmpty nums then 0
-    else match List.head nums with
-    | Num n -> n + (multiply (List.tail nums))
-    | _ -> failwith "Invalid number"
-
-let rec divide nums = 
-    if List.isEmpty nums then 1
-    else match List.head nums with
-    | Num n -> n / (divide (List.tail nums))
-    | _ -> failwith "Invalid number"
-
-let rec subtract nums = 
-    if List.isEmpty nums then 0
-    else match List.head nums with
-    | Num n -> n - (subtract (List.tail nums))
-    | _ -> failwith "Invalid number"
+let rec subtract nums =
+    if List.isEmpty nums then
+        0
+    else
+        match List.head nums with
+        | Num n -> n - (subtract (List.tail nums))
+        | _ -> failwith "Invalid number"
 
 let rec eq nums =
     let rec eqHelper fv nums =
-        if List.isEmpty nums then true
-        else match List.head nums with
-        | Num n -> (n = fv) && eqHelper fv (List.tail nums)
+        if List.isEmpty nums then
+            true
+        else
+            match List.head nums with
+            | Num n -> (n = fv) && eqHelper fv (List.tail nums)
+            | _ -> failwith "Invalid number"
+    if List.isEmpty nums then
+        failwith "Eq takes at least one argument"
+    else
+        match List.head nums with
+        | Num n -> (eqHelper n (List.tail nums))
         | _ -> failwith "Invalid number"
-    if List.isEmpty nums then failwith "Eq takes at least one argument"
-    else match List.head nums with
-    | Num n -> (eqHelper n (List.tail nums))
-    | _ -> failwith "Invalid number"
 
 
 let evalMath op nums =
@@ -73,12 +80,12 @@ let evalMath op nums =
     elif op = '-' then Num(subtract nums)
     elif op = '=' then Bool(eq nums)
     else failwith "Invalid Operation"
-    
-let evalApp a : Expr =
+
+let evalApp a: Expr =
     match List.head a with
     | Variable v ->
         printf "Pretending to run %s" v
-        Application (a.Head, a.Tail)
+        Application(a.Head, a.Tail)
     | Operation o -> evalMath o (List.tail a)
     | _ -> failwith "Invalid Function Name"
 
@@ -89,10 +96,10 @@ let rec eval e =
     | Bool b -> Bool b
     | Player n -> Player n
     | SavedFun f -> SavedFun f
-    | Application(ex, el) -> evalApp(eval ex::List.map eval el)
+    | Application(ex, el) -> evalApp (eval ex :: List.map eval el)
     | Variable s -> Variable s
-    | AndOp al -> Bool (evalAnd (List.map eval al))
-    | OrOp ol -> Bool (evalOr (List.map eval ol))
-    | IfOp il -> eval (evalIf ((eval (List.head il))::(List.tail il)))
+    | AndOp al -> Bool(evalAnd (List.map eval al))
+    | OrOp ol -> Bool(evalOr (List.map eval ol))
+    | IfOp il -> eval (evalIf ((eval (List.head il)) :: (List.tail il)))
     | Program p -> Program(List.map eval p)
     | Operation o -> Operation o
--- ProjectParser.fs
+++ ProjectParser.fs
@@ -3,17 +3,17 @@ module ProjectParser
 open Parser
 
 type Expr =
-| Num of int
-| Operation of char
-| AndOp of Expr list
-| OrOp of Expr list
-| IfOp of Expr list
-| Variable of string
-| Bool of bool
-| Application of Expr * Expr list
-| SavedFun of Expr
-| Player of int
-| Program of Expr list
+    | Num of int
+    | Operation of char
+    | AndOp of Expr list
+    | OrOp of Expr list
+    | IfOp of Expr list
+    | Variable of string
+    | Bool of bool
+    | Application of Expr * Expr list
+    | SavedFun of Expr
+    | Player of int
+    | Program of Expr list
 
 (* HELPER COMBINATORS *)
 
@@ -23,12 +23,7 @@ type Expr =
  * @param p   A parser.
  * @param sep A separator parser.
  *)
-let pmany2sep p sep =
-    pseq
-        p
-        (pmany1 (pright sep p))
-        (fun (x,xs) -> x::xs)
-    <!> "pmany2sep"
+let pmany2sep p sep = pseq p (pmany1 (pright sep p)) (fun (x, xs) -> x :: xs) <!> "pmany2sep"
 
 (*
  * From Course Material
@@ -37,63 +32,30 @@ let pmany2sep p sep =
  * returns.
  * @param p A parser.
  *)
-let inParens p =
-    pbetween
-        (pchar '(')
-        (pchar ')')
-        p
-    <!> "inParens"
+let inParens p = pbetween (pchar '(') (pchar ')') p <!> "inParens"
 
 (* Grammar *)
 
 let expr, exprImpl = recparser()
 
-let pTrue =
-    pstr "True" <|> pstr "true"
-    |>> (fun c -> Bool(true))
-    <!> "pTrue"
+let pTrue = pstr "True" <|> pstr "true" |>> (fun c -> Bool(true)) <!> "pTrue"
 
-let pFalse =
-    pstr "False" <|> pstr "false"
-    |>> (fun c -> Bool(false))
-    <!> "pFalse"
-    
-let pBool = 
-    pTrue
-    <|> pFalse
-    <!> "pBool"
+let pFalse = pstr "False" <|> pstr "false" |>> (fun c -> Bool(false)) <!> "pFalse"
+
+let pBool = pTrue <|> pFalse <!> "pBool"
 
 (* From Course Material *)
-let pNumber =
-    pmany1 pdigit
-    |>> (fun ds -> Num (int (stringify ds)))
-    <!> "pNumber"
+let pNumber = pmany1 pdigit |>> (fun ds -> Num(int (stringify ds))) <!> "pNumber"
 
-let pNeither =
-    pstr "Neither"
-    |>> (fun c -> Player(0))
-    <!> "Neither"
+let pNeither = pstr "Neither" |>> (fun c -> Player(0)) <!> "Neither"
 
-let pPlayer1 =
-    pstr "Player1"
-    |>> (fun c -> Player(1))
-    <!> "pPlayer1"
+let pPlayer1 = pstr "Player1" |>> (fun c -> Player(1)) <!> "pPlayer1"
 
-let pPlayer2 =
-    pstr "Player2"
-    |>> (fun c -> Player(2))
-    <!> "pPlayer2"
+let pPlayer2 = pstr "Player2" |>> (fun c -> Player(2)) <!> "pPlayer2"
 
-let pPlayer =
-    pPlayer1
-    <|> pPlayer2
-    <|> pNeither
-    <!> "pPlayer"
+let pPlayer = pPlayer1 <|> pPlayer2 <|> pNeither <!> "pPlayer"
 
-let pVariable =
-    pmany1 (pletter <|> pchar '?')
-    |>> (fun c -> Variable(stringify c))
-    <!> "pVariable"
+let pVariable = pmany1 (pletter <|> pchar '?') |>> (fun c -> Variable(stringify c)) <!> "pVariable"
 
 (*
  * Parses a specific operation in parenthases
@@ -101,76 +63,30 @@ let pVariable =
  * of parameters.
  * @param op a string representing function name.
  *)
-let funCall op =
-    inParens
-        (pright (pstr (op + " ")) (pmany2sep expr pws1))
-    <!> "funCall"
-        
-let pAnd =
-    funCall "and"
-    |>> (fun a -> AndOp(a))
-    <!> "pAnd"
+let funCall op = inParens (pright (pstr (op + " ")) (pmany2sep expr pws1)) <!> "funCall"
 
-let pOr =
-    funCall "or"
-    |>> (fun a -> OrOp(a))
-    <!> "pOr"
+let pAnd = funCall "and" |>> (fun a -> AndOp(a)) <!> "pAnd"
 
-let pIf =
-    funCall "if"
-    |>> (fun r -> IfOp(r))
-    <!> "pIf"
+let pOr = funCall "or" |>> (fun a -> OrOp(a)) <!> "pOr"
 
-let pSpecialForm =
-    pAnd
-    <|> pIf
-    <|> pOr
+let pIf = funCall "if" |>> (fun r -> IfOp(r)) <!> "pIf"
 
-let pOp =
-    pchar '+'
-    <|> pchar '/'
-    <|> pchar '-'
-    <|> pchar '*'
-    <|> pchar '='
-    |>> (fun c -> Operation(c))
-    <!> "pOp"
+let pSpecialForm = pAnd <|> pIf <|> pOr
+
+let pOp = pchar '+' <|> pchar '/' <|> pchar '-' <|> pchar '*' <|> pchar '=' |>> (fun c -> Operation(c)) <!> "pOp"
 
 
-let pFunctionName =
-    pOp
-    <|> pVariable
-    <!> "pFunctionName"
+let pFunctionName = pOp <|> pVariable <!> "pFunctionName"
 
 let pApplication =
-    inParens
-        (pseq
-            pFunctionName
-            (pmany0 (pright pws1 expr))
-            (fun (c,d) -> Application(c,d)))
-    <!> "pApplication"
+    inParens (pseq pFunctionName (pmany0 (pright pws1 expr)) (fun (c, d) -> Application(c, d))) <!> "pApplication"
 
-let pSavedFun =
-    pseq
-        (pchar '\'')
-        pApplication
-        (fun (c,d) -> SavedFun(d))
-    <!> "pSavedFun"
+let pSavedFun = pseq (pchar '\'') pApplication (fun (c, d) -> SavedFun(d)) <!> "pSavedFun"
 
-exprImpl :=
-    pSpecialForm
-    <|> pApplication
-    <|> pSavedFun
-    <|> pNumber
-    <|> pPlayer
-    <|> pBool
-    <|> pVariable
-    <!> "expr"
+exprImpl := pSpecialForm <|> pApplication <|> pSavedFun <|> pNumber <|> pPlayer <|> pBool <|> pVariable <!> "expr"
 
 let manyExpr =
-    pleft
-        (pseq expr (pmany0 (pright pws1 expr)) (fun (x,xs) -> Program(x::xs)))
-        pws0
-    <!> "manyExpr"
-   
+    pleft (pseq expr (pmany0 (pright pws1 expr)) (fun (x, xs) -> Program(x :: xs))) pws0 <!> "manyExpr"
+
 
 let grammar = pleft manyExpr peof <!> "grammar"
