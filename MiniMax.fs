40b7e5827b28fa7428735f6d62e79d3dd5d3b2bc
Whit Jack <whit@whitjack.me>
2020-05-14 15:29:29 -0700

    Minimax

--- MiniMax.fs
+++ MiniMax.fs
@@ -2,12 +2,46 @@ module MiniMax
 
 open ProjectParser
 
-type Max = {Board:List; Children: Min list option; Player: Player; Value: int}
-type Min = {Board:List; Children: Max list option; Player: Player; Value: int}
+let childGen children =
+    if List.isEmpty children then None else Some children
 
-let valueFun x = 0
+type MaxTree(board: Expr, children: MinTree list, value: int) =
+    member this.Board = board
+    member this.Children = childGen children
+    member this.Player = Player(1)
+    member this.Value = value
 
-let rec value (gt:GameTree) =
-    if List.isEmpty gt.Children
-    then valueFun Board
-        
+and MinTree(board: Expr, children: MaxTree list, value: int) =
+    member this.Board = board
+    member this.Children = childGen children
+    member this.Player = Player(2)
+    member this.Value = value
+
+let validMoves (initBoard: 'a): 'b list = failwith "no implementation"
+
+let minInt = -2147483648
+let maxInt = 2147483647
+
+let checkWin (board: Expr) state: Expr = failwith "No implementation"
+
+let minValue (children: MaxTree list option): int =
+    match children with
+    | Some l ->
+        List.fold (fun acc (x: MaxTree) ->
+            if x.Value < acc then x.Value else acc) maxInt l
+    | None -> minInt
+
+let maxValue (children: MinTree list option): int =
+    match children with
+    | Some l ->
+        List.fold (fun acc (x: MinTree) ->
+            if x.Value > acc then x.Value else acc) minInt l
+    | None -> maxInt
+
+let rec genMaxTree (board: Expr) =
+    let children = List.map genMinTree (validMoves board)
+    MaxTree(board, children, maxValue (childGen children))
+
+and genMinTree (board: Expr) =
+    let children = List.map genMaxTree (validMoves board)
+    MinTree(board, children, minValue (childGen children))
