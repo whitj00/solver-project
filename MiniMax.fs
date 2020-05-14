6e55199464da88a341164c54ff8ff7297cdec717
Whit Jack <whit@whitjack.me>
2020-05-14 01:43:01 -0700

    Stash

--- MiniMax.fs
+++ MiniMax.fs
@@ -1,14 +1,13 @@
 module MiniMax
 
-open ProjectInterpreter
+open ProjectParser
 
-type GameTree = {Board:List; Children: GameTree list; Player: Player; Value: int}
-
+type Max = {Board:List; Children: Min list option; Player: Player; Value: int}
+type Min = {Board:List; Children: Max list option; Player: Player; Value: int}
 
 let valueFun x = 0
 
 let rec value (gt:GameTree) =
     if List.isEmpty gt.Children
     then valueFun Board
-    else
-    
+        
